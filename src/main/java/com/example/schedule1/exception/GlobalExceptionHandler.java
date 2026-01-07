7505adb1e8c9692a2be723f5c1cf76ba53b4678d
youzting <158013348+youzting@users.noreply.github.com>
2026-01-07 20:13:03 +0900

    클라이언트 에러메세지

--- src/main/java/com/example/schedule1/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/schedule1/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler  {
@@ -13,5 +14,15 @@ public class GlobalExceptionHandler  {
         return ResponseEntity
                 .status(ex.getStatus())
                 .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValid(MethodArgumentNotValidException ex) {
+        String msg = ex.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(e -> e.getDefaultMessage())
+                .orElse("입력 값이 올바르지 않습니다.");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
     }
 }
